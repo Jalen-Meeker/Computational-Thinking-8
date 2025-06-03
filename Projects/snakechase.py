1710a9ad84b891b4cb206e54a5288d04b0e56773
Jalen-Meeker <jalenmeeker@seattleacademy.org>
2025-06-03 20:12:58 +0000

    turn in

--- Projects/snakechase.py
+++ Projects/snakechase.py
@@ -1,11 +1,11 @@
- Section 1 - Helper functions (DON'T CHANGE!!)
+#Section 1 - Helper functions (DON'T CHANGE!!)
 import turtle, math, time, random
 def set_background(image_filename):
 	screen = turtle.Screen()
 	try:
-    	screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
 	except:
-    	screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
 def set_image(sprite, image_filename):
 	image_file = f"/workspaces/Computational-Thinking-8/Images/{image_filename}.gif"
 	screen = turtle.Screen()
@@ -26,8 +26,8 @@ window.tracer(0)
 
 # Section 2: Setup
 s1 = create_sprite ("corgi", -100,0)
-set_backround ("pond")
-lives = 3  # Setting the starting value for the variable 'lives'
+set_background ("pond")
+lives = 3  # Setting the starting value for the variable 'lives' 
 
 # Section 3: Controls
 
@@ -47,10 +47,12 @@ def move_right():
     s1.setheading(0)
     s1.forward(10)
 
-window.onkeypress(move_up, "w")
-window.onkeypress(move_down, "s")
-window.onkeypress(move_left, "a")
-window.onkeypress(move_right, "d")
+window.onkeypress(move_up, "w") #shows that when the player presses w the character will move up
+window.onkeypress(move_down, "s") #shows that when the player presses s the character will move down
+window.onkeypress(move_left, "a") #shows that when the player presses a the character will move left
+window.onkeypress(move_right, "d") #shows that when the player presses d the character will move right
+s2 = create_sprite ("fox" ,-250, 0) 
+
 # Section 4: Game Loop
 window.listen()
 timer = 0
@@ -59,20 +61,21 @@ while True:
 	timer += 1  
 	 
     
- 	
-	s2 =create fox
-	s2_move left
-	if get_distance (s1,s2) < 50
-		live - = 1
+ 
+	movement = random.randint (-3, 4)
+	s2.forward (movement)
+
+	if get_distance (s1,s2) < 50:
+		live -= 1
 
 
 
 
 	window.update()
 
-	  if : lives = 0
+	if lives == 0:
 		break
 	
 
-print("gameover")
+print("game over")
 	
